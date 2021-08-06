28ff496da615ca5331845bc420df58fa05662bd9
Andrea Corrado <andrea.corrado@ucdconnect.ie>
2021-08-06 22:12:27 +0200

    paper up to glmer

--- master/pacakges_install.R
+++ master/pacakges_install.R
@@ -3,7 +3,15 @@
 pkgs <- installed.packages()[,1]
 
 # packages for the code
-useful <- c('ggplot2', 'reshape2', 'deSolve', 'minpack.lm','gam', 'xtable','e1071')
-
+useful <- c(
+  'ggplot2',
+  'reshape2',
+  'deSolve', 
+  'minpack.lm',
+  'gam',
+  'xtable',
+  'e1071',
+  'viridis'
+  )
 # install those not available
 for (p in useful) if (!(p %in% pkgs)) install.packages(p)
