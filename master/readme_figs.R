a01e5ed50fd25f7594d0f506154f6a6a3f6260ab
Andrea Corrado <andrea.corrado@ucdconnect.ie>
2021-08-14 12:00:22 +0200

    load_occourrence added

--- master/readme_figs.R
+++ master/readme_figs.R
@@ -1,10 +1,34 @@
 # readme_figs.R
 
-library(plotly)
-today <- Sys.Date()
-tm <- seq(0, 600, by = 10)
-x <- today - tm
-y <- rnorm(length(x))
-fig <- plot_ly(x = ~x, y = ~y, mode = 'lines', text = paste(tm, "days from today"))
+# script to create png figures
 
-fig
+library(gganimate)
+
+# load climate data
+X <- read.csv('../data/climate_pred.csv')[,-1]
+X <- X[complete.cases(X),]
+
+# create fake group for coloring
+group <- factor(rep('2', nrow(X)), levels = c(0,1,2))
+
+# create plot for temperature
+p <-
+  ggplot(X, aes(x = year, y=temp_obs, color = group) ) + # add axis
+  geom_line() + # add line 
+  scale_color_manual(values=c("red")) + # add color 
+  theme(legend.position="none") + # remove legend
+  labs(x = "Year", y = 'Temperature') # add labels
+p
+
+p + transition_reveal(year) 
+
+# create plot for temperature
+p <-
+  ggplot(X, aes(x = year, y=rain_obs, color = group) ) + # add axis
+  geom_line() + # add line 
+  scale_color_manual(values=c("red")) + # add color 
+  theme(legend.position="none") + # remove legend
+  labs(x = "Year", y = 'Rain') # add labels
+p
+
+p + transition_reveal(year) 
